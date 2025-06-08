d1a3129e7c36c762b8596912beb28b30e9fc1dc1
tontonxan <tontonxan>
2025-06-08 23:56:04 +0200

    Update indent

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,24 +1,25 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	dependencies = {
-		"nmac427/guess-indent.nvim",
-	},
-	main = "ibl",
-	event = "VeryLazy",
-	config = function()
-		require("ibl").setup({
-			scope = {
-				show_start = false,
-			},
-			indent = {
-				char = "┊",
-				tab_char = "┊",
-				smart_indent_cap = true,
-			},
-			whitespace = {
-				remove_blankline_trail = true,
-			},
-		})
-		require("guess-indent").setup({})
-	end,
+  "lukas-reineke/indent-blankline.nvim",
+  dependencies = {
+    "nmac427/guess-indent.nvim",
+  },
+  main = "ibl",
+  event = "VeryLazy",
+  config = function()
+    require("ibl").setup({
+      scope = {
+        enabled = true,
+        show_start = false,
+      },
+      indent = {
+        char = "┊",
+        tab_char = "┊",
+        smart_indent_cap = true,
+      },
+      whitespace = {
+        remove_blankline_trail = true,
+      },
+    })
+    require("guess-indent").setup({})
+  end,
 }
