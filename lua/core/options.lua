37d046e3846efc3134a9059400cec26a07b6e6ff
tontonxan <tontonxan>
2025-06-15 21:23:02 +0200

    Add plugins
    - blink
    - lazydev
    - lsp-config
    - telescope (same as fzf)

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,16 +6,16 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2        -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2     -- 2 spaces for indent width
-opt.expandtab = true   -- expand tab to spaces
-opt.autoindent = true  -- copy indent from current line when starting a new one
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting a new one
 
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case sensitive
 
 opt.cursorline = true
 
@@ -23,7 +23,7 @@ opt.cursorline = true
 -- (true color terminal needed)
 opt.termguicolors = true
 -- opt.background = "dark" -- colorscheme that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of file or insert mode start position
@@ -40,27 +40,27 @@ opt.listchars:append({ eol = "↵", tab = "  ", trail = "~", space = "·" })
 opt.list = true
 
 -- set vertical marker
-opt.colorcolumn = '80'
+opt.colorcolumn = "80"
 
 if vim.fn.has("wsl") == 1 then
-    if vim.fn.executable("wl-copy") == 0 then
-        print("wl-clipboard not found, clipboard integration won't work")
-    else
-        vim.g.clipboard = {
-            name = "wl-clipboard (wsl)",
-            copy = {
-                ["+"] = 'wl-copy --foreground --type text/plain',
-                ["*"] = 'wl-copy --foreground --primary --type text/plain',
-            },
-            paste = {
-                ["+"] = (function()
-                    return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', {''}, 1) -- '1' keeps empty lines
-                end),
-                ["*"] = (function()
-                    return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', {''}, 1)
-                end),
-            },
-            cache_enabled = true
-        }
-    end
+  if vim.fn.executable("wl-copy") == 0 then
+    print("wl-clipboard not found, clipboard integration won't work")
+  else
+    vim.g.clipboard = {
+      name = "wl-clipboard (wsl)",
+      copy = {
+        ["+"] = "wl-copy --foreground --type text/plain",
+        ["*"] = "wl-copy --foreground --primary --type text/plain",
+      },
+      paste = {
+        ["+"] = function()
+          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { "" }, 1) -- '1' keeps empty lines
+        end,
+        ["*"] = function()
+          return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { "" }, 1)
+        end,
+      },
+      cache_enabled = true,
+    }
+  end
 end
