0a59c4dfcb03a0c47606cb0b1a014036ee2207a1
TonTonXan <>
2025-08-21 01:42:48 +0200

    Add copy wsl and vertical marker

--- init.lua
+++ init.lua
@@ -64,6 +64,7 @@ map('n', '<leader><tab><tab>', '<cmd>tabnew<CR>')
 map('n', '<leader><tab>x', '<cmd>tabclose<CR>')
 map('n', '<leader><tab>n', '<cmd>tabn<CR>')
 map('n', '<leader><tab>p', '<cmd>tabp<CR>')
+
 map('n', '<leader><tab>f', '<cmd>tabnew %<CR>')
 
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -90,6 +91,7 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/kdheepak/lazygit.nvim" },
 })
 
 -- auto complete
@@ -111,11 +113,36 @@ map('n', '<leader>f', ":Pick files<CR>")
 map('n', '<leader>h', ":Pick help<CR>")
 map('n', '<leader>e', ":Oil<CR>")
 
-vim.lsp.enable({ "lua_ls" })
-
-
 map('n', '<leader>lg', '<cmd>LazyGit<CR>')
+
+vim.lsp.enable({ "lua_ls" })
 
 -- Color scheme
 vim.cmd("colorscheme modus")
 vim.cmd(":hi statusline guibg=NONE")
+
+-- set vertical marker
+vim.opt.colorcolumn = "80"
+
+if vim.fn.has("wsl") == 1 then
+	if vim.fn.executable("wl-copy") == 0 then
+		print("wl-clipboard not found, clipboard integration won't work")
+	else
+		vim.g.clipboard = {
+			name = "wl-clipboard (wsl)",
+			copy = {
+				["+"] = "wl-copy --foreground --type text/plain",
+				["*"] = "wl-copy --foreground --primary --type text/plain",
+			},
+			paste = {
+				["+"] = function()
+					return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { "" }, 1) -- '1' keeps empty lines
+				end,
+				["*"] = function()
+					return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { "" }, 1)
+				end,
+			},
+			cache_enabled = true,
+		}
+	end
+end
