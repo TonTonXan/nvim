48b58475173088d3ef9104b172e55fa93de7af33
TonTonXan <>
2025-09-01 23:09:00 +0200

    Update init.lua
    - Add plugins
    - Add keymaps

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ vim.o.relativenumber = true
 vim.o.termguicolors = true
 vim.o.undofile = true
 vim.o.signcolumn = "yes"
+vim.o.background = "dark"
 
 vim.o.wrap = false
 vim.o.swapfile = false
@@ -43,29 +44,24 @@ vim.o.confirm = true
 
 local map = vim.keymap.set
 
-map('n', '<Esc>', '<cmd>nohlsearch<CR>')
-map({ 'i', 'v', 'x' }, 'jk', '<ESC>')
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+map({ "i", "v", "x" }, "jk", "<ESC>")
 
-map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+map("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+map("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+map("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+map("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 map("n", "<C-h>", "<C-w><C-h>")
 map("n", "<C-l>", "<C-w><C-l>")
 map("n", "<C-j>", "<C-w><C-j>")
 map("n", "<C-k>", "<C-w><C-k>")
 
-map('n', '<leader>sv', '<C-w>v')
-map('n', '<leader>sh', '<C-w>s')
-map('n', '<leader>se', '<C-w>=')
-map('n', '<leader>sx', '<cmd>close<CR>')
+map("n", "<leader><tab><tab>", "<cmd>tabnew<CR>")
+map("n", "<leader><tab>x", "<cmd>tabclose<CR>")
+map("n", "<leader><tab>n", "<cmd>tabn<CR>")
+map("n", "<leader><tab>p", "<cmd>tabp<CR>")
 
-map('n', '<leader><tab><tab>', '<cmd>tabnew<CR>')
-map('n', '<leader><tab>x', '<cmd>tabclose<CR>')
-map('n', '<leader><tab>n', '<cmd>tabn<CR>')
-map('n', '<leader><tab>p', '<cmd>tabp<CR>')
-
-map('n', '<leader><tab>f', '<cmd>tabnew %<CR>')
+map("n", "<leader><tab>f", "<cmd>tabnew %<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking",
@@ -75,13 +71,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-map('n', '<leader>o', ':update<CR> :source<CR>')
-map('n', '<leader>w', ':write<CR>')
-map('n', '<leader>q', ':quit<CR>')
-map('n', '<leader>lf', vim.lsp.buf.format)
+map("n", "<leader>o", ":update<CR> :source<CR>")
+map("n", "<leader>w", ":write<CR>")
+map("n", "<leader>q", ":quit<CR>")
+map("n", "<leader>lf", vim.lsp.buf.format)
 
-map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+map({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
+map({ "n", "v", "x" }, "<leader>d", '"+d<CR>')
+
+map("n", "<leader>ge", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end)
+
+map("n", "<leader>gE", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end)
 
 vim.pack.add({
 	{ src = "https://github.com/miikanissi/modus-themes.nvim" },
@@ -89,34 +93,130 @@ vim.pack.add({
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/neovim/nvim-lspconfig.git" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/kdheepak/lazygit.nvim" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/rcarriga/nvim-notify" },
+	{ src = "https://github.com/tpope/vim-sleuth" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/Saghen/blink.cmp", version = "v1.6.0" },
 })
 
+-- treesitter
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
+	ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "htmldjango" },
+	sync_install = false,
+	auto_install = true,
+	ignore_install = {},
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
+
+-- lsp
+require("mason").setup()
+vim.lsp.enable({ "lua_ls", "pyright" })
+
+-- blink.cmp
+require("blink.cmp").setup({
+	{
+		keymap = { preset = "default" },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = { documentation = { auto_show = false } },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		fuzzy = { implementation = "rust_prefered" },
+	},
+	opts_extend = { "sources.default" },
+})
+
+-- telescope
+local builtin = require("telescope.builtin")
+
+function SearchClasses()
+	builtin.lsp_dynamic_workspace_symbols({
+		symbols = { "Class" },
+		prompt_title = "Search Classes",
+	})
+end
+
+function SearchFunctions()
+	builtin.lsp_dynamic_workspace_symbols({
+		symbols = { "Function", "Method" },
+		prompt_title = "Search Functions",
+	})
+end
+
+function SearchVariables()
+	builtin.lsp_dynamic_workspace_symbols({
+		symbols = { "Variable", "Constant" },
+		prompt_title = "Search Variables",
+	})
+end
+
+vim.keymap.set("n", "<leader>se", builtin.find_files, {})
+vim.keymap.set("n", "<leader>so", builtin.oldfiles, {})
+-- vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
+vim.keymap.set("n", "<leader>sf", SearchFunctions, {})
+vim.keymap.set("n", "<leader>sc", SearchClasses, {})
+vim.keymap.set("n", "<leader>sv", SearchVariables, {})
+vim.keymap.set("n", "<leader>ss", builtin.lsp_dynamic_workspace_symbols, {})
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>sq", builtin.quickfix, {})
+vim.keymap.set("n", "<leader>sk", builtin.keymaps, {})
+
+-- conform
+require("conform").setup({
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		json = { "jq" },
+		rust = { "rustfmt" },
+		python = { "black" },
+		htmldjango = { "djlint" },
+		html = { "djlint" },
+		javascript = { "prettier" },
+	},
+})
+
+-- lualine
+require("lualine").setup({})
+
 -- auto complete
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
+		if client:supports_method("textDocument/completion") then
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
 	end,
 })
 vim.cmd("set completeopt+=noselect")
 
-require "mini.pick".setup()
-require "oil".setup()
-require "mason".setup()
+require("mini.pick").setup()
+require("oil").setup()
 
-map('n', '<leader>f', ":Pick files<CR>")
-map('n', '<leader>h', ":Pick help<CR>")
-map('n', '<leader>e', ":Oil<CR>")
+map("n", "<leader>f", ":Pick files<CR>")
+map("n", "<leader>h", ":Pick help<CR>")
+map("n", "<leader>e", ":Oil<CR>")
 
-map('n', '<leader>lg', '<cmd>LazyGit<CR>')
-
-vim.lsp.enable({ "lua_ls" })
+map("n", "<leader>lg", "<cmd>LazyGit<CR>")
 
 -- Color scheme
 vim.cmd("colorscheme modus")
