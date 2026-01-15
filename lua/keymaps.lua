81fa6560b77f86b109e4f7c8e39667d9b9a280df
TonTonXan <aurelien.durance@outlook.com>
2026-01-15 23:36:09 +0100

    Update format command

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -17,10 +17,24 @@ map('n', '<leader>sx', '<cmd>close<CR>', { desc = "Close split" })
 
 map('n', '<leader>w', ':write<CR>', { desc = "Write" })
 map('n', '<leader>q', ':quit<CR>', { desc = "Quit" })
-map('n', '<leader>lf', vim.lsp.buf.format, { desc = "Format" })
+-- map('n', '<leader>lf', vim.lsp.buf.format, { desc = "Format" })
 
 -- Lazygit
 map('n', '<leader>lg', '<cmd>LazyGit<CR>', { desc = "Open LazyGit" })
 
 -- Oil
 map('n', '<leader>e', ":Oil<CR>", { desc = "Open Oil" })
+
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
+map('n', '<leader>lf', '<cmd>Format<CR>')
